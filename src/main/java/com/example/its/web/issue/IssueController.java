0621683b34a2531a083f9c2ee3eb4407e4489e01
matchan1201 <bumpakano.1@gmail.com>
2022-08-04 01:16:03 +0900

    JAVA-4 validationの作成まで

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -2,22 +2,46 @@ package com.example.its.web.issue;
 
 import com.example.its.domain.issue.IssueEntity;
 import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/issues")
+@RequiredArgsConstructor
 public class IssueController {
 
-    private final IssueService issueService = new IssueService();
+    private final IssueService issueService;
 
     // GET /issues
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model) {
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
+    }
+
+    //GET /issues/creationForm
+    @GetMapping("/creationForm")
+    public String showCreateForm(@ModelAttribute IssueForm form) {
+        return "issues/creationForm";
+    }
+
+    // POST /issues
+    @PostMapping
+    public String create(@Validated IssueForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return  showCreateForm(form);
+        }
+        issueService.create(form.getSummary(), form.getDescription());
+        return "redirect:/issues";
     }
 
 }
